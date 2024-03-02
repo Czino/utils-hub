7de7ac5f93dba3770267b1afdcd2bb637bf3af01
Czino <eric@czino.de>
2024-03-02 19:09:27 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import react from '@astrojs/react'
-import { defineConfig } from 'astro/config'
-
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: 'https://utils-hub.com',
+  integrations: [react(), tailwind(), sitemap()],
 })
